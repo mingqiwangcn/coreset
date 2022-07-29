730e84f364dad1f7d5103fe54f71fd673437c00d
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-07-29 16:44:52 -0500

    forgetting done

---
+++ forgetting.py
@@ -0,0 +1,81 @@
+import logging
+#logging.basicConfig(level=logging.ERROR)
+import json
+from tqdm import tqdm
+import random
+import os
+import argparse
+import uuid
+import train_reader
+from src.options import Options 
+
+def get_train_opt():
+    options = Options()
+    options.add_reader_options()
+    options.add_optim_options()
+    opt = options.parse()
+    data_dir = 'output/forgetting'
+    if not os.path.isdir(data_dir):
+        os.makedirs(data_dir)
+    percentage = 5
+    train_name = 'train_%d' % percentage
+    train_file = '../fusion_reader/open_domain_data/NQ/coreset/train_data_percent_%d.jsonl' % percentage
+    opt.train_data = train_file
+    opt.eval_data = '../fusion_reader/open_domain_data/NQ/coreset/dev_data.jsonl'
+    opt.model_size= 'base'
+    opt.per_gpu_batch_size = 1 
+    opt.n_context = 10
+    opt.name=train_name
+    opt.checkpoint_dir=data_dir
+    opt.use_auto_steps = 1
+    opt.epoch_ckp_num = 1 
+    return opt
+
+data_stat = {}
+prev_acc_key = 'prev_acc'
+forgetting_key = 'forgetting'
+
+def update_forgettings(qid, acc):
+    if qid not in data_stat:
+        data_stat[qid] = {
+            'qid':qid,
+            prev_acc_key:0,
+            forgetting_key:0
+        }
+    item = data_stat[qid]
+    if item[prev_acc_key] > acc:
+        item[forgetting_key] += 1
+    item[prev_acc_key] = acc
+
+class CoresetMethod:
+    def __init__(self, out_dir):
+        self.call_back = train_reader.evaluate_train
+        #self.out_dir = out_dir
+        #self.counter = 0
+
+    def do(self, dataset, idxes, coreset_metrics):
+        for offset, idx in enumerate(idxes):
+            qid = dataset.get_example(idx)['qid']
+            acc = coreset_metrics[offset] 
+            update_forgettings(qid, acc)
+    
+def main():
+    opt = get_train_opt()
+    out_dir = os.path.join(opt.checkpoint_dir, opt.name)
+    if os.path.isdir(out_dir):
+        print('%s already exists' % out_dir)
+        return
+    method = CoresetMethod(out_dir)
+    train_reader.main(opt, coreset_method=method)
+    write_stat(out_dir)
+
+def write_stat(out_dir):
+    out_stat_file = os.path.join(out_dir, 'forgetting.jsonl')
+    with open(out_stat_file, 'w') as f_o:
+        for qid in tqdm(data_stat):
+            item_stat = data_stat[qid]
+            f_o.write(json.dumps(item_stat) + '\n')
+
+if __name__ == '__main__':
+    main()
+
