59f598e3c7844a73b5207b445e0916630b0df384
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-08-10 15:44:10 -0500

    compute forgetting statistics

--- data_stat.py
+++ data_stat.py
@@ -1,6 +1,7 @@
 import json
 import glob
 import csv
+import numpy as np
 
 def get_report():
     #data_map = read_data()
@@ -41,12 +42,17 @@ def read_data():
     return data_map
 
 def gen_forgetting_data(step):
+    update_cnt_lst = [] 
+    forgetting_lst = []
     data_learnable = []
     data_unlearnable = []
     data_file = './output/forgetting/train_5/forgetting_step_%d.jsonl' % step
     with open(data_file) as f:
         for line in f:
             item = json.loads(line)
+            update_cnt_lst.append(item['update_cnt'])
+            if item['forgetting'] > 0:
+                forgetting_lst.append(item['forgetting'])
             if item['first_correct_step'] is not None:
                 data_learnable.append(item)
             else:
@@ -59,7 +65,23 @@ def gen_forgetting_data(step):
     with open('./output/forgetting/train_5/forgetting_sorted.jsonl', 'w') as f_o:
         for item in out_data:
             f_o.write(json.dumps(item) + '\n')
+    
+    update_cnt_min = np.min(update_cnt_lst)
+    update_cnt_max = np.max(update_cnt_lst)
+    update_cnt_mean = np.mean(update_cnt_lst)
+    update_cnt_std = np.std(update_cnt_lst)
+    update_cnt_median = np.median(update_cnt_lst)
 
+    print('update_cnt_min=%d, update_cnt_max=%d, update_cnt_mean=%d, update_cnt_std=%d, update_cnt_median=%d' % (
+           update_cnt_min, update_cnt_max, update_cnt_mean, update_cnt_std, update_cnt_median))
+
+    print('forgetting_min=%d, forgetting_max=%d, forgetting_mean=%d, forgetting_std=%d, forgetting_median=%d' % (
+           np.min(forgetting_lst),
+           np.max(forgetting_lst),
+           np.mean(forgetting_lst),
+           np.std(forgetting_lst),
+           np.median(forgetting_lst))
+    )
 
 def gen_coreset(coreset_tag, up_to_rows, strategy_func):
     data_file = '../data/NQ/coreset/train_data_percent_5.jsonl'
@@ -95,11 +117,19 @@ def use_unlearnable_only(data):
             qid_set.add(item['qid'])
     return qid_set
 
+def use_learnable_only(data):
+    qid_set = set()
+    for item in data:
+        if item['first_correct_step'] is not None:
+            qid_set.add(item['qid'])
+    return qid_set
+
 def main():
-    #gen_forgetting_data(11874) 
+    gen_forgetting_data(11874) 
     #gen_coreset('fg_gt_0', None, remove_zero_forgetting)
     #gen_coreset('fg_unlearnable', None, use_unlearnable_only)
-    get_report()
+    #gen_coreset('fg_learnable', None, use_learnable_only)
+    #get_report()
 
 if __name__ == '__main__':
     main()
