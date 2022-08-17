2e73122d7fc1a93040e28eda5e3e8c42cfdbb0f9
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-08-17 11:29:17 -0500

    add dataset arg

--- train_random.sh
+++ train_random.sh
@@ -1,17 +1,17 @@
-if [ "$#" -ne 1 ]; then
-    echo "Usage: ./train.sh <tag>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./train.sh <dataset> <tag>"
     exit
 fi
-
-tag=$1
+dataset=$1
+tag=$2
 python ../fusion_reader/train_reader.py \
-        --train_data ../data/NQ/coreset/train_data_p_5_num_${tag}.jsonl \
-        --eval_data ../data/NQ/coreset/dev_data.jsonl \
+        --train_data ../data/${datset}/coreset/train_data_p_5_num_${tag}.jsonl \
+        --eval_data ../data/${dataset}/coreset/dev_data.jsonl \
         --model_size base \
         --per_gpu_batch_size 1 \
         --n_context 10 \
         --name train_random_p_5_${tag} \
-        --checkpoint_dir ./output \
+        --checkpoint_dir ./output/forgetting/${dataset}/ \
         --use_auto_steps 1 \
         --epoch_ckp_num 2 \
         --max_epoch 5 \
